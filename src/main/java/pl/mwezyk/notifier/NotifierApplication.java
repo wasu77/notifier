a679c778822e2bc2c2e823b9c15bb39e1581c48f
mwezyk <maciek.wezyk@gmail.com>
2021-03-28 14:30:54 +0200

    empty spring boot project

---
+++ src/main/java/pl/mwezyk/notifier/NotifierApplication.java
@@ -0,0 +1,19 @@
+package pl.mwezyk.notifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifierApplication {
+
+    public static void main(String[] args) {
+
+        String s = "";
+
+        s.charAt(1);
+
+
+        SpringApplication.run(NotifierApplication.class, args);
+    }
+
+}
